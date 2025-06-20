d04173d38320e4db7a316a784c5048f8c0e0543b
Valentin <latronixo@gmail.com>
2025-06-20 19:01:42 +0300

    2/9. Демонстрация использования .navigationDestination

--- 43.Navigation/ContentView.swift
+++ 43.Navigation/ContentView.swift
@@ -6,27 +6,28 @@
 //
 
 import SwiftUI
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
-    }
-}
+//
+//struct DetailView: View {
+//    var number: Int
+//    
+//    var body: some View {
+//        Text("Detail View \(number)")
+//    }
+//    
+//    init(number: Int) {
+//        self.number = number
+//        print("Creating detail view \(number)")
+//    }
+//}
 
 struct ContentView: View {
     var body: some View {
         NavigationStack {
             List(0..<1000) { i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
             }
         }
     }
