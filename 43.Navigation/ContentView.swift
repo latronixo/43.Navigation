bfe83cd694bc40d52b5bde67de993009b8fc7289
Valentin <latronixo@gmail.com>
2025-06-20 20:16:31 +0300

    5/9. Переход к новым окнам, значения которых будут произвольными целыми числами

--- 43.Navigation/ContentView.swift
+++ 43.Navigation/ContentView.swift
@@ -7,35 +7,24 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path = [Int]()
     
     var body: some View {
         NavigationStack (path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
                 }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .toolbar{
-                Button("Push 556") {
-                    path.append(556)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
         }
     }
 }
