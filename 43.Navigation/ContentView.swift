06cc69d87b648ae31679ddbc30e8bc5ede0f74b0
Valentin <latronixo@gmail.com>
2025-06-20 21:47:43 +0300

    6/9. Демонстрация сохранения пути навигации используя Codable (JSON-файл) и массив целочисленных значений

--- 43.Navigation/ContentView.swift
+++ 43.Navigation/ContentView.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+//Класс, в котором будем сохранять весь путь навигации между окнами
+@Observable
+class PathStore {
+    //путь навигации
+    var path: [Int] {
+        didSet {
+            //при каждом изменении массива навигации будем сохранять в JSON файле (чтобы восстановить после закрытия приложения)
+            save()
+        }
+    }
+    
+    //путь сохранения
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    //в инициализаторе будем извлекать из JSON-файла
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        //если же декодировать из файла не получилось, то массив пути будет пустой
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
 struct DetailView: View {
     var number: Int
     @Binding var path: NavigationPath
