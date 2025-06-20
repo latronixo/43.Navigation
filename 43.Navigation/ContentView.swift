80912fcede54cc8076424be5ee1a84b675280cd0
Valentin <latronixo@gmail.com>
2025-06-20 20:31:49 +0300

    5/9. Возврат на "Главную" при помощи присваивания переменной path пустого значения NavigationPath()

--- 43.Navigation/ContentView.swift
+++ 43.Navigation/ContentView.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 struct DetailView: View {
     var number: Int
-    @Binding var path: [Int]
+    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
             .toolbar {
                 Button("Home") {
-                    path.removeAll()
+                    path = NavigationPath()
                 }
             }
     }
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack (path: $path) {
