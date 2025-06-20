87d84b773938b03ddc7d0faf00a04f012fddd2a9
Valentin <latronixo@gmail.com>
2025-06-20 16:21:24 +0300

    Initial Commit

---
+++ 43.Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  43.Navigation
+//
+//  Created by Валентин on 20.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 43.Navigation/_3_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_NavigationApp.swift
+//  43.Navigation
+//
+//  Created by Валентин on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _3_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
