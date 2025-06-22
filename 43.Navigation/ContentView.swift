1b8c847c15dbabad611100ce513914c581e36079
Valentin <latronixo@gmail.com>
2025-06-22 13:56:35 +0300

    9/9. Заголовок навбара можно сделать редактируемым пользователем, используя привязку

--- 43.Navigation/ContentView.swift
+++ 43.Navigation/ContentView.swift
@@ -8,18 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title = "SwiftUI"
+    
     var body: some View {
         NavigationStack {
             Text("Hello, world!")
-            .toolbar {
-                ToolbarItemGroup(placement: .topBarLeading) {
-                    Button("Tap Me") {
-                    }
-                
-                    Button("Or tap Me") {
-                    }
-                }
-            }
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
